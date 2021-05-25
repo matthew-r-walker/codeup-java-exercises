296ccba5fba48a8b90d9cad609aab93b8dff35f9
Matthew Walker <matthew.r.walker4@gmail.com>
2021-05-25 15:40:13 -0500

    completed alternative fizzbuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,21 +1,42 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        // Instead of Fizzbuzz
+        /*You have to design the code such that the user has only three tries to guess the correct pin of the account.
+        You set the pin as a constant with a final attribute. When correct display "Correct, welcome back."
+        When incorrect display "Incorrect, try again.". When user runs out of tries display
+        "Sorry but you have been locked out."
 
-        // while loop exercise refactored with a for loop
-//        for (int i = 5; i <= 15; ++i) {
-//            System.out.print(i);
-//        }
-        // do while loop exercise refactored with a for loop
-//        for (int i = 0; i <= 100; i += 2) {
-//            System.out.printf("The value of i is: %s%n", i);
-//        }
+            program Starts:
+            Please enter pin:
+            22132
+            Output:
+            Incorrect, try again.
+            Please enter pin:
+            23412
+            Output:
+            Incorrect, try again.
+            Please enter pin:
+            12345
+            Output:
+            Correct, welcome back.
+             */
+        Scanner scanner = new Scanner(System.in);
 
-//        for (int i = 100; i >= 0; i -= 5) {
-//            System.out.printf("The value of i is: %s%n", i);
-//        }
+        final int PIN = 12345;
 
-        for (long i = 2; i < 1000000; i *= i) {
-            System.out.printf("The value of i is: %s%n", i);
+        for (int i = 1; i <= 3; ++i) {
+            System.out.println("Please enter pin:");
+            int userInputPin = scanner.nextInt();
+            if (i == 3 && userInputPin != PIN) {
+                System.out.println("Sorry but you have been locked out");
+            } else if (userInputPin == PIN) {
+                System.out.println("Correct, welcome back.");
+                i = 3;
+            } else if (userInputPin != PIN) {
+                System.out.println("Incorrect, try again.");
+            }
         }
     }
 }
