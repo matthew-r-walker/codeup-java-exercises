9c0729fc8660b88458db0f74ad3fcd7104dfb939
Matthew Walker <matthew.r.walker4@gmail.com>
2021-05-25 14:21:49 -0500

    completed do while loop exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,22 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("The value of i is: " + i);
-            ++i;
-        }
+//        int i = 0;
+//        do {
+//            System.out.printf("The value of i is %s%n", i);
+//            i += 2;
+//        } while (i <= 100);
+
+//        int i = 100;
+//        do {
+//            System.out.printf("The value of i is %s%n", i);
+//            i -= 5;
+//        } while (i >= -10);
+
+        long i = 2;
+        do {
+            System.out.printf("The value of i is %s%n", i);
+            i *= i;
+        } while (i < 1000000);
+
     }
 }
