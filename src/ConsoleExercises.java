b281b3142ab0f9584cbb93de9b9deada42cd5462
Matthew Walker <matthew.r.walker4@gmail.com>
2021-05-25 14:06:23 -0500

    completed while loop exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,21 +7,20 @@ public class ConsoleExercises {
 
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Enter a number.");
-//        int userInputNum = scanner.nextInt();
-//        System.out.printf("Your number is: %s%n", userInputNum);
-//
-//        System.out.println("Enter three words.");
-//        String firstUserWord = scanner.next();
-//        String secondUserWord = scanner.next();
-//        String thirdUserWord = scanner.next();
-//        System.out.printf("Your first word is %s%nYour second word is %s%nYour third word is %s%n", firstUserWord, secondUserWord, thirdUserWord);
-//
-//        System.out.println("Enter a sentence.");
-//        scanner.nextLine();
-//        String userSentence = scanner.nextLine();
-//        System.out.printf("The sentence you entered is: %s%n", userSentence);
+        System.out.println("Enter a number.");
+        int userInputNum = scanner.nextInt();
+        System.out.printf("Your number is: %s%n", userInputNum);
 
+        System.out.println("Enter three words.");
+        String firstUserWord = scanner.next();
+        String secondUserWord = scanner.next();
+        String thirdUserWord = scanner.next();
+        System.out.printf("Your first word is %s%nYour second word is %s%nYour third word is %s%n", firstUserWord, secondUserWord, thirdUserWord);
+
+        System.out.println("Enter a sentence.");
+        scanner.nextLine();
+        String userSentence = scanner.nextLine();
+        System.out.printf("The sentence you entered is: %s%n", userSentence);
 
 
         System.out.println("What is the length, width and height of your room?");
