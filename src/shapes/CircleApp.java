9a79aff467c51b21f879c3eae500953d2f61da5f
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-02 09:47:03 -0500

    completed circle exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -4,10 +4,12 @@ import util.Input;
 
 public class CircleApp {
     public static void main(String[] args) {
+
         Input in = new Input();
+        System.out.println("Enter the radius of a circle.");
         Circle circle = new Circle(in.getDouble());
-        System.out.println(circle.getArea());
-        System.out.println(circle.getCircumference());
+        System.out.println("The area of your circle is: " + circle.getArea());
+        System.out.println("The circumference of your circle is: " + circle.getCircumference());
 
     }
 }
