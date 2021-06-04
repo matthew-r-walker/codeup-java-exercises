041b687b28cc13e6fe7ea5cc04afa21de02ec41c
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-04 14:02:20 -0500

    changed output message to show less decimal numbers and added notes

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,12 +5,12 @@ public class ShapesTest {
     public static void main(String[] args) {
 
         Rectangle box1 = new Rectangle(5,4);
-        System.out.printf("Rectangle length is: %f width is: %f%n", box1.length, box1.width);
+        System.out.printf("Rectangle length is: %.2f width is: %.2f%n", box1.length, box1.width);
         System.out.println(box1.getPerimeter());
         System.out.println(box1.getArea());
 
         Rectangle box2 = new Square(5);
-        System.out.printf("Square side length is: %f%n", box2.length);
+        System.out.printf("Square side length is: %.2f%n", box2.length);
         System.out.println(box2.getPerimeter());
         System.out.println(box2.getArea());
 
