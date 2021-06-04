81bd5e2f36366a35dbe21d5361e4b3d44106b82c
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-03 20:11:39 -0500

    completed arrays exercise and server name generator exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -14,7 +14,7 @@ public static Person[] addPerson(Person[] arr, Person addPerson) {
 
 
         int[] numbers = {1, 2, 3, 4, 5};
-        // we need Arrays.toString to show the data within the numbers array because numbers alone just refers to the array object.
+// we need Arrays.toString to show the data within the numbers array because numbers alone just refers to the array object.
         System.out.println(Arrays.toString(numbers));
 
 
@@ -38,8 +38,10 @@ public static Person[] addPerson(Person[] arr, Person addPerson) {
         morePeople[1] = person5;
 
         morePeople = addPerson(morePeople, person1);
+        morePeople = addPerson(morePeople, person2);
+        morePeople = addPerson(morePeople, person3);
 
-        addPerson(people, person4);
+
 
 
         for (Person person : morePeople) {
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,14 @@
 public class ServerNameGenerator {
+   private static String[] adjective = {"adorable", "adventurous", "aggressive", "agreeable", "alert", "alive", "amused", "angry", "annoyed", "annoying"};
+   private static String[] noun = {"lion", "tiger", "bear", "dog", "cat", "alligator", "cricket", "bird", "wolf", "snake"};
+
+
+    public static String randomEle(String[] arr) {
+        int rand = (int)(Math.random() * arr.length);
+        return arr[rand];
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Here is your server name:%n%s-%s",randomEle(adjective), randomEle(noun));
+    }
 }
