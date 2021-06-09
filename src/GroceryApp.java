db887923badd464b35dfb3d4f61525779a77a92b
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-09 00:37:26 -0500

    saved progress on grocery app

--- src/GroceryApp.java
+++ src/GroceryApp.java
@@ -1,17 +1,15 @@
 import util.Input;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class GroceryApp {
 
     public static String[] groceryCats = {
-        "Baby",
-        "Beverages",
-        "Bread & Bakery",
-        "Breakfast & Cereal",
-        "Canned Goods & Soups",
-        "Condiments/Spices & Bake",
-        "Cookies, Snacks & Candy",
+//        "Baby",
+//        "Beverages",
+//        "Bread & Bakery",
+//        "Breakfast & Cereal",
         "Dairy, Eggs & Cheese",
         "Deli",
         "Frozen Foods",
@@ -22,20 +20,87 @@ public class GroceryApp {
     public static Input input = new Input();
 
     public static Scanner sc = new Scanner(System.in);
-    public static void makeGroceryList() {
+
+    public static HashMap<String, HashMap<String, Integer>> gList = new HashMap<>();
+
+    public static void makeIt() {
         System.out.println("Would you like to create a grocery list?");
         if (input.yesNo()) {
-            System.out.println("Which category would you like to choose from?");
-            for (int i = 0; i < groceryCats.length; ++i) {
-                System.out.println(i + 1 + ". " + groceryCats[i]);
-            }
+            makeGroceryList();
         }
-        int userNum = input.getInt(1, 12);
-        System.out.println(groceryCats[userNum -1]);
+    }
+    /// Currently souts multiple times if I continue to add more items to list by calling the function recursively
+    public static void makeGroceryList() {
+        System.out.println("Which category would you like to choose from?");
+        for (int i = 0; i < groceryCats.length; ++i) {
+            System.out.println(i + 1 + ". " + groceryCats[i]);
+        }
+
+        int userNum = input.getInt(1, groceryCats.length);
+        String userChoice = groceryCats[userNum -1];
+        System.out.println(userChoice);
+
+        if (!gList.containsKey(userChoice)) {
+            addCat(userChoice);
+        }
+
+        HashMap<String, Integer> selectedCat = gList.get(userChoice);
+
+        do {
+            addItemOrUpdateItemVal(selectedCat);
+            System.out.println("Would you like to add another item from " + userChoice);
+        } while (input.yesNo());
+
+        System.out.println("Would you like to add more to the list?");
+        if (input.yesNo()) {
+            makeGroceryList();
+        }
+
+        System.out.println(gList);
+    }
+
+    public static void addItemOrUpdateItemVal(HashMap<String, Integer> items) {
+        System.out.println("What item would you like to add?");
+        String userInpItem = sc.nextLine();
+        System.out.println("How many " + userInpItem + " would you like to add?");
+        int userNum = input.getInt();
+        if (items.containsKey(userInpItem)) {
+            int oldVal = items.get(userInpItem);
+            items.replace(userInpItem, oldVal + userNum);
+        } else {
+            items.put(userInpItem, userNum);
+        }
+    }
+
+    public static void addCat(String key) {
+        if (!gList.containsKey(key)) {
+            gList.put(key, new HashMap<String, Integer>());
+        }
     }
 
 
     public static void main(String[] args) {
-        makeGroceryList();
+//        HashMap<String, Integer> items = new HashMap<>();
+//        items.put("Banana", 7);
+//        gList.put(groceryCats[4], items);
+
+        makeIt();
+//        items.put("Strawberries", 7);
+//
+//        addItemOrUpdateItemVal(items);
+
+//        System.out.println(items.get("Banana"));
+
+//        updateVal(items, "Banana", 4);
+
+//        System.out.println(items.get("Banana"));
+
+        // this will display what is in the produce hash map key value
+//        System.out.println(gList.get("Produce"));
+
+        // this will display the value of strawberries within the hash map key value within the produce hashmap key value
+//        System.out.println(gList.get("Produce").get("Strawberries"));
+//        System.out.println(gList.get("Produce").keySet());
+
     }
 }
